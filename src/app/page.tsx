bebe10594ba932c8e8dc558f2f943cf551aeecf0
Rainer Kuhn <ticktricktrack@gmail.com>
2026-04-27 23:25:22 +0100

    add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
-import Image from "next/image";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Testing var lock secrets: {process.env.TEST_TOKEN}
-          </p>
+    <div>
+      {/* <Navbar /> */}
 
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Testing var lock secrets: {process.env.PASSWORD_TEST}
-          </p>
-        </div>
-      </main>
+      <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+              Testing var lock secrets: {process.env.TEST_TOKEN}
+            </p>
+
+            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+              Testing var lock secrets: {process.env.PASSWORD_TEST}
+            </p>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,37 @@
+import { SunIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Logo } from "@/components/logo";
+import { NavMenu } from "@/components/nav-menu";
+import { NavigationSheet } from "@/components/navigation-sheet";
+
+const Navbar = () => {
+  return (
+    <nav className="h-16 border-b bg-background">
+      <div className="mx-auto flex h-full max-w-(--breakpoint-xl) items-center justify-between px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center gap-12">
+          <Logo />
+
+          {/* Desktop Menu */}
+          <NavMenu className="hidden md:block" />
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button className="hidden sm:inline-flex" variant="outline">
+            Sign In
+          </Button>
+          <Button>Sign Up</Button>
+          <Button size="icon" variant="outline">
+            <SunIcon />
+          </Button>
+
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            <NavigationSheet />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
